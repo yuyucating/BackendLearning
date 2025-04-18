5d2cf9c6c517eb5f0627e014fbeb58de8ef62407
Una <yuyucating@gmail.com>
2025-04-18 22:05:08 +0800

    第3堂課結束!

--- 250418/Main.java
+++ 250418/Main.java
@@ -2,15 +2,15 @@ public class Main {
     public static void main(String[] args){
 
         double toy = 37;
-        int itemCount = 2;
+        int itemCount = 4;
         double totalPrice;
         double TAX_RATE = 0.05;
 
         double coupon = 15;
-        boolean isDiscount = false;
+        boolean isDiscount = true;
 
 
-        if (isDiscount) {
+        if (isDiscount && itemCount>3) {
             totalPrice = (toy * itemCount - coupon) * (1 + TAX_RATE);
             System.out.print("總金額: " + totalPrice);
         } else {
