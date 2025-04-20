40ddfada6c4c32e304790e6015a7c5763eff6a94
Una <yuyucating@gmail.com>
2025-04-20 11:04:52 +0800

    使用 scanner 輸入參數

--- 250419/Main_1.java
+++ 250419/Main_1.java
@@ -1,11 +1,22 @@
+import java.util.Scanner;
+
 public class Main_1 {
     public static void main(String[] args){
-        int thisYear = 1112;
-
-        if((thisYear%4==0 && thisYear%100!=0) || thisYear%400==0){
-            System.out.print(thisYear+" 是閏年");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("請輸入年份: ");
+        // int thisYear;
+        
+        if (scanner.hasNextInt()){
+            int thisYear = scanner.nextInt();
+            System.out.println("輸入格式正確!! 您輸入的是 "+thisYear);
+            if((thisYear%4==0 && thisYear%100!=0) || thisYear%400==0){
+                System.out.print(thisYear+" 是閏年");
+            }else{
+                System.out.print(thisYear+" 不是閏年");
+            }
         }else{
-            System.out.print(thisYear+" 不是閏年");
+            System.out.println("輸入的不是整數..");
         }
+        scanner.close();
     }
 }
--- 250419/Main_2.java
+++ 250419/Main_2.java
@@ -1,15 +1,26 @@
+import java.util.Scanner;
+
 public class Main_2 {
     public static void main(String[] args){
         // BMI = 身高(m)^2/體重(kg)
-        double theH = 1.67;
-        double theW = 66.9336;
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("請輸入身高(m): ");
+        double theH = scn.nextDouble();
+        // System.out.println(theH);
+        System.out.print("請輸入體重(kg): ");
+        double theW = scn.nextDouble();
+        // System.out.println(theW);
+
         double theBMI = theW/Math.pow(theH, 2);
         String theResult;
-        // System.out.print(theBMI);
+
         if(theBMI<18.5){theResult = "過輕";}
         else if(18.5<=theBMI && theBMI<24){theResult = "正常";}
         else{theResult = "過重";}
 
         System.out.printf("BMI=%.1f, %s", theBMI, theResult);
+
+        scn.close();
     }
 }
