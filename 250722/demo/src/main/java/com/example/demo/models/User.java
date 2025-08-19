49599b2974b155c6b21b3cc06c2610c1570dbbbe
Una <yuyucating@gmail.com>
2025-08-19 21:54:35 +0800

    ROM 關聯式:在@Entity 加 @Column (自動加 database 欄位)

--- 250722/demo/src/main/java/com/example/demo/models/User.java
+++ 250722/demo/src/main/java/com/example/demo/models/User.java
@@ -22,6 +22,9 @@ public class User {
     //todo 實際應用環境切勿使用明碼儲存
     private String password;
 
+    @Column(name="role")
+    private String role;
+
     public User(int id, String username, String email){
         this.id = id;
         this.username = username;
@@ -58,6 +61,14 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 
 }
